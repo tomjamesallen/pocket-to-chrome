0f55f7e9531ce23d3990f2e9378d5c1fb080f9ce
Tom Allen <tomjamesallenweb@gmail.com>
2018-07-07 11:34:50 +0100

    nodemon set up, adding babel + `babel-plugin-transform-class-properties`

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
   entry: {
-    background: './src/background.js',
+    background: ['babel-polyfill', './src/background.js'],
   },
   output: {
     filename: '[name].js',
